d4e9fd6681f5c5a7fb07e36d1ec9c84e8c0fae61
meadowdeath <galgudez@gmail.com>
2026-04-11 23:25:12 -0600

    feat(basic-data-types): add FindTheRunnerUpScore solution

---
+++ basic-data-types/FindTheRunnerUpScore.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+    first = float('-inf')
+    second = float('-inf')
+
+    for num in arr:
+        if num > first:
+            second = first
+            first = num
+        elif num > second and num != first:
+            second = num
+
+    print(second)
