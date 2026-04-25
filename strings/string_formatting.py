7ecb041f75e3db93d1ca5878cdb779a354d91cd0
meadowdeath <galgudez@gmail.com>
2026-04-24 23:22:13 -0600

    feat(strings): add string_formatting solution

---
+++ strings/string_formatting.py
@@ -0,0 +1,15 @@
+def print_formatted(number):
+    # your code goes here
+    width = len(bin(number)[2:])
+    
+    for i in range(1, number + 1):
+        
+        decimal = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexadecimal = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+        print(f"{decimal} {octal} {hexadecimal} {binary}")
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
