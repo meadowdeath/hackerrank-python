2fc450323edc242eb8a82cff4d401fa3b9e65dcc
meadowdeath <galgudez@gmail.com>
2026-05-08 23:19:07 -0600

    feat(sets): add set_symmetric_difference solution

---
+++ sets/set_symmetric_difference.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n = input()
+    english_subbed = set(map(int, input().split()))
+    b = input()
+    french_subbed = set(map(int, input().split()))
+    
+    sym_difference_subbed = english_subbed.symmetric_difference(french_subbed)
+    
+    print(len(sym_difference_subbed))
