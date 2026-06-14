370265679896517019f37ff207d6008455484efc
meadowdeath <galgudez@gmail.com>
2026-06-13 19:41:37 -0600

    feat(collections): add piling_up solution

---
+++ collections/piling_up.py
@@ -0,0 +1,28 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import deque
+
+def can_pile(cubes):
+
+    d = deque(cubes)
+    last_picked = float('inf')
+
+    while d:
+
+        if d[0] >= d[-1]:
+            current = d.popleft()
+        else:
+            current = d.pop()
+
+        if current > last_picked:
+            return "No"
+
+        last_picked = current
+
+    return "Yes"
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        cubes = list(map(int, input().split()))
+        print(can_pile(cubes))
