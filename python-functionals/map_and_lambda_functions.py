9b392690ed3781f97eac895d88adc1b315bc32b5
meadowdeath <galgudez@gmail.com>
2026-06-29 23:23:09 -0600

    feat(python-functional): add map_and_lambda_functions solution

---
+++ python-functionals/map_and_lambda_functions.py
@@ -0,0 +1,12 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    fib_list = [0, 1]
+    for _ in range(2, n):
+        fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib_list[:n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
