86d5c6bc7e37819005ee10b6dfdb1c6419402481
meadowdeath <galgudez@gmail.com>
2026-07-03 23:44:05 -0600

    feat(regex-and-parsing): add detect_floating_point_number solution

---
+++ regex-and-parsing/detect_floating_point_number.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+if __name__ == '__main__':
+    
+    for _ in range(int(input())):
+        print(bool(re.match(r'^[+-]?\d*\.\d+$', input().strip())))
